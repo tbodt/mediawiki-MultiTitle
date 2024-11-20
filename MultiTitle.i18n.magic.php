90add2037411b5e5b2a9c1b3e80119ce6cd958e5
Theodore Dubois <tblodt@icloud.com>
2024-11-19 23:45:18 -0800

    Initial commit

---
+++ MultiTitle.i18n.magic.php
@@ -0,0 +1,11 @@
+<?php
+
+$magicWords = [];
+
+$magicWords['en'] = [
+	'keeptitle' => [ 0, '__KEEPTITLE__' ],
+];
+
+$magicWords['tok'] = [
+	'keeptitle' => [ 0, '__NIMI_SULI_O_AWEN__', '__O_AWEN_E_NIMI_SULI__' ],
+];
---
+++ includes/Hooks.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MediaWiki\Extension\MultiTitle;
+
+use MediaWiki\Hook\GetDoubleUnderscoreIDsHook;
+use MediaWiki\Page\Hook\ArticleViewFooterHook;
+use MediaWiki\Page\Hook\ArticleViewRedirectHook;
+use MediaWiki\Page\PageProps;
+
+class Hooks implements GetDoubleUnderscoreIDsHook, ArticleViewRedirectHook, ArticleViewFooterHook {
+	private PageProps $mPageProps;
+
+	public function __construct( PageProps $pageProps ) {
+		$this->mPageProps = $pageProps;
+	}
+
+	public function onGetDoubleUnderscoreIDs( &$ids ) {
+		$ids[] = 'keeptitle';
+	}
+
+	private function getPageProperty( $title, $propertyName ) {
+		if ( !$title ) {
+			return null;
+		}
+		$properties = $this->mPageProps->getProperties( $title, $propertyName );
+		return $properties[$title->getId()] ?? null;
+	}
+
+	public function onArticleViewRedirect( $article ) {
+		if ( $this->getPageProperty( $article->getRedirectedFrom(), 'keeptitle' ) !== null ) {
+			return false;
+		}
+	}
+
+	public function onArticleViewFooter( $article, $patrolFooterShown ) {
+		$redirectTitle = $article->getRedirectedFrom();
+		if ( $this->getPageProperty( $redirectTitle, 'keeptitle' ) === null ) {
+			return;
+		}
+
+		$outputPage = $article->getContext()->getOutput();
+		$redirectDisplayTitle = $this->getPageProperty( $redirectTitle, 'displaytitle' ) ?? $redirectTitle;
+		$outputPage->setPageTitle( $redirectDisplayTitle );
+		$outputPage->setDisplayTitle( $redirectDisplayTitle );
+		$outputPage->addSubtitle( $article->getTitle() );
+	}
+}

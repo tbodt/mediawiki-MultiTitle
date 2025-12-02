f09764c30253a4033d85a230d460563acf882e10
Theodore Dubois <tblodt@icloud.com>
2025-12-02 09:44:33 -0800

    Make magic word case sensitive

--- MultiTitle.i18n.magic.php
+++ MultiTitle.i18n.magic.php
@@ -3,9 +3,9 @@
 $magicWords = [];
 
 $magicWords['en'] = [
-	'keeptitle' => [ 0, '__KEEPTITLE__' ],
+	'keeptitle' => [ 1, '__KEEPTITLE__' ],
 ];
 
 $magicWords['tok'] = [
-	'keeptitle' => [ 0, '__NIMI_SULI_O_AWEN__', '__O_AWEN_E_NIMI_SULI__' ],
+	'keeptitle' => [ 1, '__NIMI_SULI_O_AWEN__', '__O_AWEN_E_NIMI_SULI__' ],
 ];
